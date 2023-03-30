6c2d262d54b26c7ed2cb13e439d1ec29a74f9457
shamiul <anik.savar.bd@gmail.com>
2023-03-30 21:04:03 +0600

    Code cleanup, design update, navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../../assets/images/Logo.svg';
+import './Navbar.css';
+
+const Header = () => {
+  return (
+    <nav className='navbar'>
+      <div><img src={Logo} alt="Logo" /></div>
+      <div>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/order">Order</a>
+          </li>
+          <li className="nav-item">
+            <a href="/review">Order Review</a>
+          </li>
+          <li className="nav-item">
+            <a href="/inventory">Manage Inventory</a>
+          </li>
+          <li className="nav-item">
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
