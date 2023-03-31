03cea89e036e404079ef84de71835a9141c9a074
shamiul <anik.savar.bd@gmail.com>
2023-03-31 19:29:08 +0600

    Showing local storage items to order history

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,11 +15,22 @@ const Shop = () => {
     .then(data => setProducts(data))
   }, []);
 
-  // Get Products from Local Storage
+  // Get Added Products from Local Storage
   useEffect( () => {
-    const storedCart = getShoppingCart();
-    console.log(storedCart);
-  }, [])
+    const storedCart = getShoppingCart(); // getting stored product's id from local storage
+    const savedCart = [];
+    // Looping through all the id's of stored cart to get the related product from the API/JSON file.
+    for(const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id); // finding the products with matched id
+      if(addedProduct) {
+        const quantity = storedCart[id]; // It indicates the stored quantity of products in local storage. 
+        addedProduct.quantity = quantity; // Using the previous information of quantity in local storage addedProduct's quantity also updated.
+        savedCart.push(addedProduct);
+      }
+    }
+    setCartItem(savedCart);
+
+  }, [products]); // adding products as dependency
 
   // Adding Products to the Cart
   const handleAddToCart = (product) => {
