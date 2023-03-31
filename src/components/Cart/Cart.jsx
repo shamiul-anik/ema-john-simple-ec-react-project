4b894c19925f8f7b677b001fcf8a5ca40e49eb38
shamiul <anik.savar.bd@gmail.com>
2023-03-31 17:29:00 +0600

    Code clean up, loading data from shopping cart, navbar sticky and cart position fixed design update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,23 +4,37 @@ import { faTrashCan, faListCheck } from '@fortawesome/free-solid-svg-icons';
 import './Cart.css';
 
 const Cart = ({cartItem}) => {
+
+  // Calculating Total Price and Total Shipping Charge
+  let totalPrice = 0;
+  let totalShippingCharge = 0;
+  for (const product of cartItem) {
+    totalPrice += product.price;
+    totalShippingCharge += product.shipping;
+  }
+
+  // Calculating Total Tax
+  const totalTax = ((totalPrice * 5)/100);
+  
+  // Calculating Grand Total
+  const grandTotal = totalPrice + totalShippingCharge + totalTax;
+
   return (
     <div className="cart-container">
-      
       <h5 className="order-summary">Order Summary</h5>
-      <div className="order-info">
+      <div className="order-details">
         <p>Selected Items: {cartItem.length}</p>
-        <p>Total Price: $</p>
-        <p>Total Shipping Charge: $</p>
-        <p>Tax: $</p>
-        <h6>Grand Total: $</h6>
+        <p>Total Price: ${totalPrice}</p>
+        <p>Total Shipping Charge: ${totalShippingCharge}</p>
+        <p>Tax: ${totalTax.toFixed(2)}</p>
+        <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
       </div>
       <div className="order-button-container">
-        <button className="btn-clear-cart">
+        <button className="btn-clear-cart" type="button">
           Clear Cart
           <FontAwesomeIcon icon={faTrashCan} beatFade />
         </button>
-        <button className="btn-review-order">
+        <button className="btn-review-order" type="button">
           Review Order
           <FontAwesomeIcon icon={faListCheck} bounce />
         </button>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,22 +2,31 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cartItem, setCartItem] = useState([]);
 
+  // Get Products from API/JSON File
   useEffect( () => {
     fetch('./productsData/products.json')
     .then(res => res.json())
     .then(data => setProducts(data))
   }, []);
 
+  // Get Products from Local Storage
+  useEffect( () => {
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
+  }, [])
+
   // Adding Products to the Cart
   const handleAddToCart = (product) => {
     const newItem = [...cartItem, product];
     setCartItem(newItem);
-  }
+    addToDb(product.id); // adding product to local storage
+  };
 
   return (
     <div className="shop-container">
